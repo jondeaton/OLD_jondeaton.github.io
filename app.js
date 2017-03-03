512c4b28e5c34371a36a9ea9488215b947ccb625
jondeaton <jonpauldeaton@gmail.com>
2017-03-02 21:57:44 -0800

    Added resume

--- app.js
+++ app.js
@@ -35,6 +35,19 @@ var query_number = function() {
     document.getElementById("query_response").textContent = words_text;
 };
 
+// Function for opening a pdf
+function openPdf(e, path, redirect) {
+    // stop the browser from going to the href
+    e = e || window.event; // for IE
+    e.preventDefault();
+
+    // launch a new window with your PDF
+    window.open(path, 'somename', ... /* options */);
+
+    // redirect current page to new location
+    window.location = redirect;
+}
+
 
 var main = function() {
 	console.log("main function called");
